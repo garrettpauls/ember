a1bb68d964d2cbfcfbefa25d891b2751fc13da9f
Garrett Pauls <garrett.pauls@gmail.com>
2016-03-05 19:59:03 -0700

    Added implementation of IMutableDependencyResolver for an Autofac container.

---
+++ CSharp/ReactiveUI/Autofac/AutofacDependencyResolver.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autofac;
+using Autofac.Core;
+
+using Splat;
+
+namespace Ember.ReactiveUI.Autofac
+{
+    public class AutofacDependencyResolver : IMutableDependencyResolver
+    {
+        private readonly IContainer mContainer;
+
+        public AutofacDependencyResolver(IContainer container)
+        {
+            mContainer = container;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public object GetService(Type serviceType, string contract = null)
+        {
+            try
+            {
+                return string.IsNullOrEmpty(contract)
+                           ? mContainer.Resolve(serviceType)
+                           : mContainer.ResolveNamed(contract, serviceType);
+            }
+            catch(DependencyResolutionException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType, string contract = null)
+        {
+            try
+            {
+                var enumerableType = typeof(IEnumerable<>).MakeGenericType(serviceType);
+                object instance = string.IsNullOrEmpty(contract)
+                                      ? mContainer.Resolve(enumerableType)
+                                      : mContainer.ResolveNamed(contract, enumerableType);
+                return ((IEnumerable) instance).Cast<object>();
+            }
+            catch(DependencyResolutionException)
+            {
+                return null;
+            }
+        }
+
+        public void Register(Func<object> factory, Type serviceType, string contract = null)
+        {
+            var builder = new ContainerBuilder();
+            if(string.IsNullOrEmpty(contract))
+            {
+                builder.Register(x => factory()).As(serviceType).AsImplementedInterfaces();
+            }
+            else
+            {
+                builder.Register(x => factory()).Named(contract, serviceType).AsImplementedInterfaces();
+            }
+            builder.Update(mContainer);
+        }
+
+        public IDisposable ServiceRegistrationCallback(Type serviceType, string contract, Action<IDisposable> callback)
+        {
+            // this method is not used by RxUI
+            throw new NotImplementedException("This method is not expected to be used by RxUI");
+        }
+    }
+}
