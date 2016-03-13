352657616b5188be74bf7f252c678c886b6ecd49
Garrett Pauls <garrett.pauls@gmail.com>
2016-03-13 13:31:53 -0600

    Added RoutableViewModel with generic host screen

--- CSharp/ReactiveUI/MVVM/RoutableViewModel.cs
+++ CSharp/ReactiveUI/MVVM/RoutableViewModel.cs
@@ -4,15 +4,25 @@ using ReactiveUI;
 
 namespace Ember.ReactiveUI.MVVM
 {
-    public abstract class RoutableViewModel : DisposableViewModel, IRoutableViewModel, IDisposable
+    public abstract class RoutableViewModel : RoutableViewModel<IScreen>
     {
-        protected RoutableViewModel(string urlPathSegment, IScreen hostScreen)
+        protected RoutableViewModel(string urlPathSegment, IScreen hostScreen) : base(urlPathSegment, hostScreen)
+        {
+        }
+    }
+
+    public abstract class RoutableViewModel<THostScreen> : DisposableViewModel, IRoutableViewModel, IDisposable
+        where THostScreen : IScreen
+    {
+        protected RoutableViewModel(string urlPathSegment, THostScreen hostScreen)
         {
             UrlPathSegment = urlPathSegment;
             HostScreen = hostScreen;
         }
 
-        public IScreen HostScreen { get; }
+        IScreen IRoutableViewModel.HostScreen => HostScreen;
+
+        public THostScreen HostScreen { get; }
 
         public string UrlPathSegment { get; }
     }
